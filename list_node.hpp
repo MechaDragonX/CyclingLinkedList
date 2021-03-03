66db44a95853959b2634261019569e503e6a6e4d
MechaDragonX <theofficalrv@gmail.com>
2021-03-02 16:11:10 -0800

    Add ListNode struct

---
+++ list_node.hpp
@@ -0,0 +1,9 @@
+#ifndef LIST_NODE_HPP
+#define LIST_NODE_HPP
+
+struct ListNode {
+    int value;
+    ListNode* next;
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "list_node.hpp"
+
+int main() {
+    /*
+        1. Check the current node
+        2. If the current address is in the address Vector, then return true
+        3. Otherwise, move on to the next one and repeat
+        4. Return false at the end if loop was never exited
+    */
+}
