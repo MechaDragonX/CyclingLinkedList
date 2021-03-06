ad10fb40d54954b9b9d2304980f9a2bcdde01888
MechaDragonX <theofficalrv@gmail.com>
2021-03-05 19:52:41 -0800

    Test linked list using struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "list_node.hpp"
+using namespace std;
 
 int main() {
     /*
@@ -8,4 +9,22 @@ int main() {
         3. Otherwise, move on to the next one and repeat
         4. Return false at the end if loop was never exited
     */
+    ListNode* link = new ListNode{
+        1, new ListNode{2, 
+            new ListNode{3, 
+                new ListNode{4, 
+                    new ListNode{5, 
+                        nullptr
+                    }
+                }
+            }
+        }
+    };
+
+    ListNode* current = link;
+    while(current != nullptr) {
+        cout << current->value << " => ";
+        current = current->next;
+    }
+    cout << "nullptr" << endl;
 }
