a4eec251aaa8545f904c7a8779d0432fd76bd696
MechaDragonX <theofficalrv@gmail.com>
2021-03-05 20:01:04 -0800

    Test with test case

--- main.cpp
+++ main.cpp
@@ -8,20 +8,31 @@ int main() {
         2. If the current address is in the address Vector, then return true
         3. Otherwise, move on to the next one and repeat
         4. Return false at the end if loop was never exited
-    */
-    ListNode* link = new ListNode{
-        1, new ListNode{2, 
-            new ListNode{3, 
-                new ListNode{4, 
-                    new ListNode{5, 
-                        nullptr
-                    }
-                }
-            }
-        }
-    };
+    // */
+    // ListNode* link = new ListNode{
+    //     1, new ListNode{2, 
+    //         new ListNode{3, 
+    //             new ListNode{4, 
+    //                 new ListNode{5, 
+    //                     nullptr
+    //                 }
+    //             }
+    //         }
+    //     }
+    // };
 
-    ListNode* current = link;
+    ListNode* one = new ListNode{1, nullptr};
+    ListNode* two = new ListNode{2, nullptr};
+    ListNode* three = new ListNode{3, nullptr};
+    ListNode* four = new ListNode{4, nullptr};
+    ListNode* five = new ListNode{5, nullptr};
+    one->next = two;
+    two->next = three;
+    three->next = four;
+    four->next = five;
+    five->next = three;
+
+    ListNode* current = one;
     while(current != nullptr) {
         cout << current->value << " => ";
         current = current->next;
